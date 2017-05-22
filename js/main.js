59f245031b82228ae1b68bd34e6354b67967a107
franshu <fran.ojeda208@gmail.com>
2017-05-22 14:37:22 -0300

    desafio 5

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//Trayendonos los elementos a utilizar
+var contenedor = document.getElementsByClassName("contenedor")[0];
+var rosa = document.getElementById("rosa");
+var azul = document.getElementById("azul");
+var verde = document.getElementById("verde");
+var amarillo = document.getElementById("amarillo");
+
+//propiedades de la caja
+contenedor.style.position = "relative";
+contenedor.style.background = "DarkGray";
+contenedor.style.width = "600px";
+contenedor.style.padding = "50px;";
+
+//propiedades al CSS
+azul.style.position = "absolute";
+azul.style.top = "250px";
+azul.style.right = "0";
+
